0619e5e016c39251df98d2fd9fe2c6920c91358f
Naman1000 <91007344+Naman1000@users.noreply.github.com>
2025-03-09 19:27:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //your JS code here. If required.
 
-function toupercase{
+
 
 	let word = document.getElementbyID.(fname);
 	word.addEventListner("input" , function()  {
 		this.value = this.value.toUpperCase();
 		
 	})
-}
+
